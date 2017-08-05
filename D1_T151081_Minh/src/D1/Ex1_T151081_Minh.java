c2e4c604e1b78e09768eb12235c7c8b2d29baa54
US <US@A1M24>
2017-08-05 15:14:46 +0700

    xap xep lai Package

--- D1_T151081_Minh/src/D1/Ex1_T151081_Minh.java
+++ D1_T151081_Minh/src/D1/Ex1_T151081_Minh.java
@@ -1,3 +1,4 @@
+package D1;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
--- D1_T151081_Minh/src/D1/Mainwindow.java
+++ D1_T151081_Minh/src/D1/Mainwindow.java
@@ -1,3 +1,4 @@
+package D1;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 

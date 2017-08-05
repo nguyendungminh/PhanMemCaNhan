02ac98acb60470115d339c17358d530b36949594
US <US@A1M24>
2017-08-05 14:43:07 +0700

    Fix #1 sua loi cac cua so cung 1 vi tri

--- D1_T151081_Minh/src/Mainwindow.java
+++ D1_T151081_Minh/src/Mainwindow.java
@@ -19,51 +19,54 @@ public class Mainwindow extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					Mainwindow frame = new Mainwindow();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	// public static void main(String[] args) {
+	// EventQueue.invokeLater(new Runnable() {
+	// public void run() {
+	// try {
+	// Mainwindow frame = new Mainwindow();
+	// frame.setVisible(true);
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// });
+	// }
 
 	/**
 	 * Create the frame.
 	 */
+	int x=0;
+	int y=0;
+	int n =1;
 	public Mainwindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmNew = new JMenuItem("New");
-		
+
 		mnFile.add(mntmNew);
-		
+
 		JMenuItem mntmOpen = new JMenuItem("Open");
 		mnFile.add(mntmOpen);
-		
+
 		JMenuItem mntmSave = new JMenuItem("Save");
 		mnFile.add(mntmSave);
-		
+
 		JMenuItem mntmExit = new JMenuItem("Exit");
 		mnFile.add(mntmExit);
-		
+
 		JMenu mnEdit = new JMenu("Edit");
 		menuBar.add(mnEdit);
-		
+
 		JMenu mnTool = new JMenu("Tool");
 		menuBar.add(mnTool);
-		
+
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
 		contentPane = new JDesktopPane();
@@ -72,17 +75,22 @@ public class Mainwindow extends JFrame {
 		setContentPane(contentPane);
 		// to bit mang hinh :3
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
-		//xu li su kien
-		//su kien cho nut new
-				mntmNew.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-					JInternalFrame newWindow = new JInternalFrame("new Window1", true, true, true);
-					newWindow.setVisible(true);
-					newWindow.setSize(300, 300);
-					contentPane.add(newWindow);
-					}
-				});
-		
+		// xu li su kien
+		// su kien cho nut new
+		mntmNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JInternalFrame newWindow = new JInternalFrame("new Window"+n, true, true, true);
+				newWindow.setVisible(true);
+				newWindow.setSize(300, 300);
+				contentPane.add(newWindow);
+				newWindow.toFront();
+				newWindow.setBounds(x, y, 300, 300);
+				x=x+10;
+				y=y+30;
+				n = n+1;
+			}
+		});
+	
 	}
 
 }

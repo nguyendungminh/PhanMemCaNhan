1bb0542b26095fed154af90ee5dac4101f669f60
US <US@A1M24>
2017-08-12 16:52:36 +0700

    them bai tap D4

--- D4_T151081_Minh/src/NotePad/MyTextEditor.java
+++ D4_T151081_Minh/src/NotePad/MyTextEditor.java
@@ -5,10 +5,12 @@ package NotePad;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 import javax.swing.JColorChooser;
@@ -31,13 +33,12 @@ public class MyTextEditor extends JFrame {
 	 */
 
 	JMenuBar mnbar;
-	JMenu mnuFile, mnuFormat;
-	JMenuItem mniOpen, mniExit, mniChangeBgColor, mniChangeFontColor;
+	JMenu mnuFile, mnuFormat, mnuTool;
+	JMenuItem mniOpen, mniExit, mniChangeBgColor, mniChangeFontColor, mniAnalyze;
 
 	JTextArea txaContent;
 	JScrollPane scrPane;
 
-	
 	/**
 	 * @param args
 	 */
@@ -65,6 +66,82 @@ public class MyTextEditor extends JFrame {
 					changeBGColor();
 				} else if (e.getSource() == mniChangeFontColor) {
 					changeFontColor();
+				} else if (e.getSource() == mniAnalyze) {
+					// cach1
+					// for (String line : txaContent.getText().split("\r\n")) {
+					// System.out.print(line);
+					// }
+					int CountSv = 0;
+					int diemtu7trolen = 0;
+					int diemtu45troxuong = 0;
+					double Min = 10;
+					double Max = 0;
+					double focmax =0;
+					String tenmin = null;
+					String tenmax = null;
+					String diemfoc = null;
+					String[] lines = txaContent.getText().split("\r\n");
+					
+					for (int i = 1; i < lines.length; i++) {
+						// System.out.print(lines[i]);
+						String[] fields = lines[i].split(",");
+						if (fields.length == 7) {
+							String stt = fields[0];
+							String sid = fields[1];
+							String lastName = fields[2];
+							String firstName = fields[3];
+							int foc = Integer.parseInt(fields[4]);
+							int introNW = Integer.parseInt(fields[5]);
+							int adp = Integer.parseInt(fields[6]);
+							// diem trung binh
+							CountSv++;
+							double average = (foc + introNW + adp) * 1.0 / 3;
+							DecimalFormat scoreFormat = new DecimalFormat("#0.00");
+							System.out.println(scoreFormat.format(average));
+							if (average >= 7) {
+								diemtu7trolen++;
+							}
+							if (average < 4.5) {
+								diemtu45troxuong++;
+							}
+							if (average < Min) {
+								Min = average;
+								tenmin = lastName + firstName;
+							}
+							if (average > Max) {
+								
+								Max = average;
+								tenmax = lastName + firstName;
+								
+							}
+							
+							if(foc>focmax){
+								focmax = foc;
+								diemfoc = lastName + firstName;
+								
+							}
+						}
+
+					}
+					String mess = "so luong sinh vien : " + CountSv+ "\n";
+					mess +="so luong sinh vien tren 7 :" + diemtu7trolen+"\n";
+					mess+="so luong sinh vien duoi 4.5 :" + diemtu45troxuong+"\n";
+					mess+="ten sinh vien co dtb nho nhat : " + tenmin+"\n";
+					mess+="diem trung binh la : " + Min+"\n";
+					mess+="ten sinh vien co dtb lon nhat : " + tenmax+"\n";
+					mess+="diem trung binh la : " + Max+"\n";
+					mess+="ten sinh vien co dtb FOC2 lon nhat : " + diemfoc+"\n";
+					mess+="diem trung binh la : " + focmax+"\n";
+					JOptionPane.showConfirmDialog(MyTextEditor.this, mess);
+//					System.out.println("so luong sinh vien : " + CountSv);
+//					System.out.println("so luong sinh vien tren 7 :" + diemtu7trolen);
+//					System.out.println("so luong sinh vien duoi 4.5 :" + diemtu45troxuong);
+//					System.out.println("ten sinh vien co dtb nho nhat : " + tenmin);
+//					System.out.println("diem trung binh la : " + Min);
+//					System.out.println("ten sinh vien co dtb lon nhat : " + tenmax );
+//					System.out.println("diem trung binh la : " + Max);
+//					System.out.println("ten sinh vien co dtb FOC2 lon nhat : " + diemfoc );
+//					System.out.println("diem trung binh la : " + focmax);
 				}
 			}
 		};
@@ -72,6 +149,7 @@ public class MyTextEditor extends JFrame {
 		mniExit.addActionListener(action);
 		mniChangeBgColor.addActionListener(action);
 		mniChangeFontColor.addActionListener(action);
+		mniAnalyze.addActionListener(action);
 		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -84,20 +162,23 @@ public class MyTextEditor extends JFrame {
 		mnbar = new JMenuBar();
 		mnuFile = new JMenu("File");
 		mnuFormat = new JMenu("Format");
+		mnuTool = new JMenu("Tool");
 		mniOpen = new JMenuItem("Open");
 		mniExit = new JMenuItem("Exit");
+		mniAnalyze = new JMenuItem("Analyze");
 		mniChangeBgColor = new JMenuItem("ChangeBgColor");
 		mniChangeFontColor = new JMenuItem("ChangeFontColor");
 
 		mnuFile.add(mniOpen);
 		mnuFile.addSeparator();
 		mnuFile.add(mniExit);
-
+		mnuTool.add(mniAnalyze);
 		mnuFormat.add(mniChangeBgColor);
 		mnuFormat.add(mniChangeFontColor);
 
 		mnbar.add(mnuFile);
 		mnbar.add(mnuFormat);
+		mnbar.add(mnuTool);
 
 		setJMenuBar(mnbar);
 	}
@@ -110,7 +191,7 @@ public class MyTextEditor extends JFrame {
 			try {
 				Scanner scn = new Scanner(fchOChooser.getSelectedFile());
 				while (scn.hasNextLine()) {
-					txaContent.append(scn.nextLine() + "\n");
+					txaContent.append(scn.nextLine() + "\r\n");
 
 				}
 				scn.close();
